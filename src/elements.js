c72bf897589ed383d77fee900758a42d3d21ec68
RNtaate <royhyde14@gmail.com>
2021-01-31 16:43:03 +0300

    Create displayForm function

---
+++ src/elements.js
@@ -0,0 +1,7 @@
+let displayForm = () => {
+  let newForm = '<form action="#" method="POST" class="weather-form"><input type="text" placeholder="Search city"><input type="submit"></form>'
+
+  return newForm;
+}
+
+export default displayForm;
