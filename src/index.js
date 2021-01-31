8762ce32c78f7fcfbdb34bcd5c3d8c96752fc112
RNtaate <royhyde14@gmail.com>
2021-01-31 18:01:11 +0300

    Add event Listener function to myForm variable.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "regenerator-runtime/runtime.js";
-import displayForm from '/src/elements';
+import {displayForm, displayWeatherDetails, displayNoInformation} from '/src/elements';
 import {getMyWeatherInformation, weatherDetails} from '/src/fetch';
+
+let myWeatherObj = {};
 
 let upperSection = document.querySelector('.upper-section');
 
@@ -8,3 +10,26 @@ let formDiv = document.createElement('div');
 formDiv.innerHTML = displayForm();
 
 upperSection.appendChild(formDiv);
+
+let myForm = document.querySelector('form');
+
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('You submitted something cool');
+
+  getMyWeatherInformation(myForm[0].value)
+  .then((response) => {
+    if(response.main) {
+      console.log(response);
+      myWeatherObj = weatherDetails(response);
+
+      document.querySelector('.details-div').innerHTML = displayWeatherDetails(myWeatherObj);
+    }
+    else {
+      document.querySelector('.details-div').innerHTML = displayNoInformation();
+    }
+  })
+  .catch((error) => {
+    document.querySelector('.details-div').innerHTML = displayNoInformation();
+  })
+})
