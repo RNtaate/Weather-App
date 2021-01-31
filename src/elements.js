26aeb7f878b98208d9a9de547fd6d1375c49de95
RNtaate <royhyde14@gmail.com>
2021-01-31 20:02:56 +0300

    Update displayWeatherDetails, displayNoInformation functions

--- src/elements.js
+++ src/elements.js
@@ -6,14 +6,14 @@ let displayForm = () => {
 
 let displayWeatherDetails = (obj) => {
   let detailsDiv = `<div class="weather-details-div">
-  <div class="country-name-div"><h1 class="country-heading-h1">${obj.city}, ${obj.country}</h1><img src="${obj.flag}" class="flag-img"></div>
+  <div class="country-name-div"><h2 class="country-heading-h2">${obj.city}, ${obj.country}</h2><img src="${obj.flag}" class="flag-img"></div>
 
   <div class="weather-info-div">
     <div class="weather-info-left-div temp-div">${obj.temp} &degF</div>
     <div class="weather-info-right-div">
-      <span>Weather: ${obj.weather}</span>
-      <span>Feels like: ${obj.feels_like}</span>
-      <span>Humidity: ${obj.humidity}</span>
+      <span>Weather : ${obj.weather}</span>
+      <span>Feels like : ${obj.feels_like}</span>
+      <span>Humidity : ${obj.humidity}</span>
     </div>
   </div>
 </div>`
@@ -21,8 +21,8 @@ let displayWeatherDetails = (obj) => {
   return detailsDiv;
 }
 
-let displayNoInformation = () => {
-  let par = `<p>NO information available!</p>`
+let displayNoInformation = (content) => {
+  let par = `<p class="no-info-p">${content}</p>`
 
   return par;
 }
