3bb114599be501a7b68399675875fcc754470cef
RNtaate <royhyde14@gmail.com>
2021-01-31 16:44:43 +0300

    Import functions form fetch, elements files, add weather form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('This is going to be my weather app');
+import "regenerator-runtime/runtime.js";
+import displayForm from '/src/elements';
+import {getMyWeatherInformation, weatherDetails} from '/src/fetch';
+
+let upperSection = document.querySelector('.upper-section');
+
+let formDiv = document.createElement('div');
+formDiv.innerHTML = displayForm();
+
+upperSection.appendChild(formDiv);
