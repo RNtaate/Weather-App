5621d9b29faac0614675d05f0f88448e001af1ca
RNtaate <royhyde14@gmail.com>
2021-01-31 23:24:36 +0300

    Create toggleDegrees function

--- src/elements.js
+++ src/elements.js
@@ -1,3 +1,5 @@
+import {celToFah, fahToCel} from '/src/tempConversion';
+
 let displayForm = () => {
   let newForm = '<form action="#" method="POST" class="weather-form"><input type="text" placeholder="Search city" required><input type="submit"></form>'
 
@@ -9,7 +11,16 @@ let displayWeatherDetails = (obj) => {
   <div class="country-name-div"><h2 class="country-heading-h2">${obj.city}, ${obj.country}</h2><img src="${obj.flag}" class="flag-img"></div>
 
   <div class="weather-info-div">
-    <div class="weather-info-left-div temp-div">${obj.temp} &degF</div>
+    <div class="weather-info-left-div"><span class="temp-div">${obj.temp.toFixed(1)}</span> <span class="temp-unit-span">&degF</span>
+      <div class="toggle-wrapper">
+        <input type="checkbox" class="my-checkbox" id="checkbox">
+        <label class="toggle-label" for="checkbox">
+          <b class="fas fa-moon">&degF</b>
+          <b class="fas fa-sun">&degC</b>
+          <div class="ball"></div>
+        </label>
+      </div>
+    </div>
     <div class="weather-info-right-div">
       <span>Weather : ${obj.weather}</span>
       <span>Feels like : ${obj.feels_like}</span>
@@ -27,5 +38,28 @@ let displayNoInformation = (content) => {
   return par;
 }
 
+let toggleDegrees = () => {
+  let toggle = document.querySelector('.my-checkbox');
 
-export {displayForm, displayWeatherDetails, displayNoInformation};
+  toggle.addEventListener('change', () => {
+    let changer = document.querySelector('.temp-div');
+    let units = document.querySelector('.temp-unit-span');
+    let celToggle = document.querySelector('.cel-toggle');
+  
+    if(celToggle === null) {
+      let celTemp = fahToCel(Number(changer.textContent));
+      changer.textContent = celTemp.toFixed(1);
+      units.innerHTML = '&degC';
+    }
+    else{
+      let fahTemp = celToFah(Number(changer.textContent));
+      changer.textContent = fahTemp.toFixed(1);
+      units.innerHTML = '&degF';
+    }
+
+    changer.classList.toggle('cel-toggle');
+  })
+}
+
+
+export {displayForm, displayWeatherDetails, displayNoInformation, toggleDegrees};
