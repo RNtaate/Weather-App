90847123f92e4cee49daa0a7d78eff9aa6837959
RNtaate <royhyde14@gmail.com>
2021-01-31 18:00:06 +0300

    Create displayWeatherDetails, displayNoInformation functions

--- src/elements.js
+++ src/elements.js
@@ -1,7 +1,31 @@
 let displayForm = () => {
-  let newForm = '<form action="#" method="POST" class="weather-form"><input type="text" placeholder="Search city"><input type="submit"></form>'
+  let newForm = '<form action="#" method="POST" class="weather-form"><input type="text" placeholder="Search city" required><input type="submit"></form>'
 
   return newForm;
 }
 
-export default displayForm;
+let displayWeatherDetails = (obj) => {
+  let detailsDiv = `<div class="weather-details-div">
+  <div class="country-name-div"><h1 class="country-heading-h1">${obj.city}, ${obj.country}</h1><img src="${obj.flag}" class="flag-img"></div>
+
+  <div class="weather-info-div">
+    <div class="weather-info-left-div temp-div">${obj.temp} &degF</div>
+    <div class="weather-info-right-div">
+      <span>Weather: ${obj.weather}</span>
+      <span>Feels like: ${obj.feels_like}</span>
+      <span>Humidity: ${obj.humidity}</span>
+    </div>
+  </div>
+</div>`
+
+  return detailsDiv;
+}
+
+let displayNoInformation = () => {
+  let par = `<p>NO information available!</p>`
+
+  return par;
+}
+
+
+export {displayForm, displayWeatherDetails, displayNoInformation};
